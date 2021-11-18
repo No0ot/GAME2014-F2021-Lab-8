81d578617fec9eb2e4e8b9c3a94d24c8b81c1745
Chris <tulip.chris@gmail.com>
2021-11-18 16:16:27 -0500

    Added LOS script and functionality to enemies

--- Assets/[Scripts]/EnemyController.cs
+++ Assets/[Scripts]/EnemyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [Header("Player Detection")]
+    public LOS enemyLOS;
+
     [Header("Movement")] 
     public float runForce;
     public Transform lookAheadPoint;
@@ -12,12 +15,17 @@ public class EnemyController : MonoBehaviour
     public LayerMask wallLayerMask;
     public bool isGroundAhead;
 
+    [Header("Animation")]
+    public Animator animationController;
+
     private Rigidbody2D rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        enemyLOS = GetComponent<LOS>();
+        animationController = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,7 +33,41 @@ public class EnemyController : MonoBehaviour
     {
         LookAhead();
         LookInFront();
-        MoveEnemy();
+        if (!HasLOS())
+        {
+            animationController.enabled = true;
+            animationController.Play("Run");
+            MoveEnemy();
+        }
+        else
+        {
+            animationController.enabled = false;
+        }
+    }
+    private bool HasLOS()
+    {
+        if(enemyLOS.colliderList.Count > 0)
+        {
+            if((enemyLOS.collidesWith.gameObject.CompareTag("Player")) && (enemyLOS.colliderList[0].gameObject.CompareTag("Player")))
+            {
+                return true;
+            }
+            else
+            {
+                foreach(var collider in enemyLOS.colliderList)
+                {
+                    if(collider.gameObject.CompareTag("Player"))
+                    {
+                        var hit = Physics2D.Raycast(lookInFrontPoint.position, Vector3.Normalize(collider.transform.position - lookInFrontPoint.position), 2.0f, enemyLOS.contactFilter.layerMask);
+                        if(hit.collider.gameObject.CompareTag("Player"))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     private void LookAhead()
